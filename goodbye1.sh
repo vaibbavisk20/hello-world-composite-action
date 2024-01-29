d9f69926b0956d007e63352b9bf262d930e30cdc
Vaibbavi SK <vaibbavis@microsoft.com>
2024-01-29 14:20:25 -0800

    gpt script file

--- goodbye1.sh
+++ goodbye1.sh
@@ -1,1 +1,11 @@
-"echo Goodbye" 
+#!/bin/bash
+
+# Print a message
+echo "Hello, this is a sample script executed by GitHub Actions."
+
+# Your commands go here
+# Example:
+# echo "Running some commands..."
+
+# Exit with success status
+exit 0
